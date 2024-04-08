1485e5687f8376468c77613e8b28e74919a71b91
akshai <57423537+nyouibou@users.noreply.github.com>
2024-04-08 22:28:45 +0530

    updated

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -5,7 +7,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:loginmb/view/login_screen/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -25,18 +27,16 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           actions: [
             IconButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => LoginScreen(),
-                  ),
-                  (route) => false,
-                );
-              },
-              icon: Icon(Icons.logout),
-            )
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginScreen(),
+                      ),
+                      (route) => false);
+                },
+                icon: Icon(Icons.logout))
           ],
           bottom: TabBar(
             tabs: [
@@ -55,87 +55,37 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               SizedBox(height: 20),
               Expanded(
-                child: TabBarView(
-                  children: [
-                    // Admins Tab
-                    StreamBuilder(
-                      stream: collecitonReference
-                          .where("role", isEqualTo: "admin")
-                          .snapshots(),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasError) {
-                          return Text("Error: ${snapshot.error}");
-                        } else if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else if (!snapshot.hasData) {
-                          return Text("No data");
-                        } else {
-                          print("Admins data: ${snapshot.data}");
-                          return ListView.builder(
-                            itemCount: snapshot.data!.docs.length,
-                            itemBuilder: (context, index) {
-                              final DocumentSnapshot userSnap =
-                                  snapshot.data!.docs[index];
-                              return ListTile(
-                                leading: CircleAvatar(
-                                  backgroundImage: NetworkImage(
-                                    userSnap["image"],
-                                  ),
-                                  radius: 40,
+                  child: StreamBuilder(
+                stream: collecitonReference.snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text("error");
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          //
+                          final DocumentSnapshot userSnap =
+                              snapshot.data!.docs[index];
+                          return ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                  userSnap["image"],
                                 ),
-                                title: Text(userSnap["username"]),
-                                subtitle: Text(userSnap["ph"]),
-                                trailing: Text(userSnap["role"]),
-                              );
-                            },
-                          );
-                        }
-                      },
-                    ),
-                    // Users Tab
-                    StreamBuilder(
-                      stream: collecitonReference
-                          .where("role", isEqualTo: "user")
-                          .snapshots(),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasError) {
-                          return Text("Error: ${snapshot.error}");
-                        } else if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else if (!snapshot.hasData) {
-                          return Text("No data");
-                        } else {
-                          print("Users data: ${snapshot.data}");
-                          return ListView.builder(
-                            itemCount: snapshot.data!.docs.length,
-                            itemBuilder: (context, index) {
-                              final DocumentSnapshot userSnap =
-                                  snapshot.data!.docs[index];
-                              return ListTile(
-                                leading: CircleAvatar(
-                                  backgroundImage: NetworkImage(
-                                    userSnap["image"],
-                                  ),
-                                  radius: 40,
-                                ),
-                                title: Text(userSnap["username"]),
-                                subtitle: Text(userSnap["ph"]),
-                                trailing: Text(userSnap["role"]),
-                              );
-                            },
-                          );
-                        }
-                      },
-                    ),
-                  ],
-                ),
-              ),
+                                radius: 40,
+                              ),
+                              title: Text(userSnap["username"]),
+                              subtitle: Text(userSnap["ph"]),
+                              trailing: Text(userSnap["role"]));
+                        });
+                  }
+                },
+              ))
             ],
           ),
         ),
